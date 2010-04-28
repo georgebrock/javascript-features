0878a1cb9f3726447e0658039ba36fff2de4ac97
George Brocklehurst <george.brocklehurst@gmail.com>
2010-04-28 17:03:23 +0100

    Mock the Rails.root method.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,3 +2,9 @@ require 'shoulda'
 require 'redgreen' unless ENV["TM_PID"]
 
 require 'javascript_features'
+
+module Rails
+  def self.root
+    File.join(File.dirname(__FILE__), 'fixtures')
+  end
+end
