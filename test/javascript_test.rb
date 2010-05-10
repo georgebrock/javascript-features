5187986d0ce0d24c3c3b18f8a70d82513570fa5b
George Brocklehurst <george.brocklehurst@gmail.com>
2010-05-10 23:11:10 +0100

    More realistic JavaScript test.
    The script would not be embedded in the page, instead it would be loaded from an external URL.

--- test/javascript_test.rb
+++ test/javascript_test.rb
@@ -1,25 +1,60 @@
 require File.join(File.dirname(__FILE__), *%w[test_helper])
 require 'harmony'
+require 'webrick'
 
 class JavascriptTest < Test::Unit::TestCase
   include JavascriptFeatures::Helper
+
+  class TestServlet < ::WEBrick::HTTPServlet::AbstractServlet
+    def initialize(server, index_html)
+      super server
+      @index_html = index_html
+    end
+
+    def service(req, res)
+      if req.path == '/index.html'
+        res.status = 200
+        res['Content-Type'] = 'text/html'
+        res.body = @index_html
+      elsif req.path =~ %r[^/javascripts/packaged/([^/]+).js$]
+        res.status = 200
+        res['Content-Type'] = 'text/javascript'
+        res.body = JavascriptFeatures::Compiler.compile(:package => $1)
+      else
+        res.status = 404
+        res['Content-Type'] = 'text/plain'
+        res.body = 'Not found'
+      end
+    end
+  end
 
   context 'an HTML page with a feature enabled' do
     setup do
       use_javascript_for :enabled_feature
 
-      @page = Harmony::Page.new(%Q{
+      html = %%
         <!DOCTYPE html>
         <html lang="en">
           <head>
             <title>Test page</title>
-            <script type="text/javascript">#{JavascriptFeatures::Compiler.compile(:package => 'javascript_test')}</script>
+            #{include_javascript_features('javascript_test')}
           </head>
           <body class="#{javascript_feature_classes}">
             <h1>Test page</h1>
           </body>
         </html>
-      })
+      %
+
+      javascript_server_config = {:BindAddress => '0.0.0.0', :Port => 8076}
+      @javascript_server = ::WEBrick::HTTPServer.new(javascript_server_config)
+      @javascript_server.mount('/', TestServlet, html)
+      Thread.new{ @javascript_server.start }
+
+      @page = Harmony::Page.fetch("http://#{javascript_server_config[:BindAddress]}:#{javascript_server_config[:Port]}/index.html")
+    end
+
+    teardown do
+      @javascript_server.shutdown
     end
 
     should 'initialise the enabled feature' do
@@ -27,7 +62,7 @@ class JavascriptTest < Test::Unit::TestCase
     end
 
     should 'not initialise the disabled feature' do
-      assert_equal 0, @page.execute_js('jQuery("div.disabled_feature").length')      
+      assert_equal 0, @page.execute_js('jQuery("div.disabled_feature").length')
     end
 
     should 'initialise the global feature' do
