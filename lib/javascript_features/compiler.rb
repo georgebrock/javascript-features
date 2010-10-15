a68a7949b8e2c9be8c9c2ef4efdba1b515acd6e6
George Brocklehurst <george.brocklehurst@gmail.com>
2010-10-15 10:39:45 +0100

    Extract code to build a list of files in a package

--- lib/javascript_features/compiler.rb
+++ lib/javascript_features/compiler.rb
@@ -6,14 +6,19 @@ module JavascriptFeatures
       defaults = {:minify => true, :package => 'main'}
       options = defaults.merge(options)
 
-      lib_files = [File.join(File.dirname(__FILE__), *%w[ .. .. assets jquery.js ])] + Dir.glob(Rails.root + '/public/javascripts/lib/**/*.js')
-      init_files = [File.join(File.dirname(__FILE__), *%w[ .. .. assets init.js ])]
-      feature_files = Dir.glob(Rails.root + "/public/javascripts/#{options[:package]}/**/*.js")
-
-      compiled = (lib_files + init_files + feature_files).map{|file| File.read(file) }.join("\n")
+      files = files_for_package(options[:package])
+      compiled = files.map{|file| File.read(file) }.join("\n")
       minified = JSMin.minify(compiled) if options[:minify]
 
       minified || compiled
+    end
+
+  private
+    def self.files_for_package(package)
+      lib_files = [File.join(File.dirname(__FILE__), *%w[ .. .. assets jquery.js ])] + Dir.glob(Rails.root + '/public/javascripts/lib/**/*.js')
+      init_files = [File.join(File.dirname(__FILE__), *%w[ .. .. assets init.js ])]
+      feature_files = Dir.glob(Rails.root + "/public/javascripts/#{package}/**/*.js")
+      lib_files + init_files + feature_files
     end
   end
 end
