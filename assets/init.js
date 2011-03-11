665c342e59ab820773c5443e04065a2f32fe0f58
George Brocklehurst <george.brocklehurst@gmail.com>
2011-03-11 11:52:30 +0000

    Improve scope of functions in init.js

--- assets/init.js
+++ assets/init.js
@@ -1,24 +1,25 @@
 var Features = {};
 
-Features.init = function(context, use_feature) {
-  for(var key in Features) {
-    if(Features.hasOwnProperty(key)) {
-      if(typeof Features[key].global_init === "function") {
-        Features[key].global_init(context);
-      }
-      if(typeof Features[key].init === "function" && use_feature(key)) {
-        Features[key].init(context);
+jQuery(function($) {
+
+  var init = function(context, use_feature) {
+    for(var key in Features) {
+      if(Features.hasOwnProperty(key)) {
+        if(typeof Features[key].global_init === "function") {
+          Features[key].global_init(context);
+        }
+        if(typeof Features[key].init === "function" && use_feature(key)) {
+          Features[key].init(context);
+        }
       }
     }
-  }
-};
+  };
 
-jQuery(function($) {
   $("body").ajaxComplete(function(event, xhr, options) {
     if(xhr.readyState !== 4){ return; }
     var init_features = (xhr.getResponseHeader("X-JavascriptFeatures-Init") || "").toLowerCase().split(" ");
     var context = $(options.context || document);
-    Features.init(context, function(feature) {
+    init(context, function(feature) {
       for(var i = 0; i < init_features.length; i++) {
         if(init_features[i] === feature.toLowerCase()) {
           return true;
@@ -27,7 +28,7 @@ jQuery(function($) {
     });
   });
 
-  Features.init($(document), function(feature) {
+  init($(document), function(feature) {
     return $("body").hasClass("with-js-" + feature.toLowerCase());
   });
 });
